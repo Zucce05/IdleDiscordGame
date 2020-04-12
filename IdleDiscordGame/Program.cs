651c19674e37cbfca97661a0b1721fc59570ab27
Zucce05 <Zucce05@hotmail.com>
2020-04-12 13:23:07 -0600

    Add project files.

---
+++ IdleDiscordGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdleDiscordGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
